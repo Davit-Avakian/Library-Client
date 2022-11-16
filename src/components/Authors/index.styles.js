351a12c39a81c516b93e7dfe279e644b0172798e
Davit-Avakian <102228451+Davit-Avakian@users.noreply.github.com>
2022-11-16 13:52:07 +0400

    Revert "Features"

--- src/components/Authors/index.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const AuthorsContainer = styled.div`
-  height: 90vh;
-  min-width: 100%;
-  display: flex;
-  justify-content: center;
-  gap: 5%;
-`;
